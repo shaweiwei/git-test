58a316669a71621cec55809a4e9c08f9f5e1ccb8
sww <364850679@qq.com>
2018-05-22 15:43:42 +0800

    11

--- src/main/java/com/ehcache/controller/OperationController.java
+++ src/main/java/com/ehcache/controller/OperationController.java
@@ -18,6 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -52,15 +55,9 @@ public class OperationController {
 	
 	Gson gson = new Gson();
 	
-	/* 如何通俗的理解java的堆和栈
-	 * 可以把堆理解为一家餐厅，里面有200张桌子，也就是最多能同时容纳200桌客人就餐，来一批客人就为他们安排一些桌子，如果某天来的客人特别多，超过200桌了，那就不能再接待超出的客人了。
-	 * 当然，进来吃饭的客人不可能是同时的，有的早，有的晚，先吃好的客人，老板会安排给他们结账走人，然后空出来的桌子又能接待新的客人。
-	 * 这里，堆就是餐厅，最大容量200桌就是堆内存的大小，老板就相当于GC（垃圾回收），给客人安排桌子就相当于java创建对象的时候分配堆内存，结账就相当于GC回收对象占用的空间。
-	 * 接着把栈比作一口废井，这口井多年不用已经没水了，主人现在把它作为贮存自酿酒的地方，存酒的时候就用绳子勾着酒坛子慢慢放下去，后面再存就一坛一坛堆着放上去，取酒的时候就先取最上面的坛子。
-	 * 这个比喻可能不太好，但是栈就是这个概念，先进后出。
-	*/
 	private String device;
 	
+	// ---------------------------------------------------------------------
 	/**
 	 * 模拟并发请求ClassVariable
 	 * @param threadNum 开启的线程数
@@ -117,7 +114,7 @@ public class OperationController {
 		return device;
 	}
 	
-	
+	// ---------------------------------------------------------------------
 	/**
 	 * 测试并发插入
 	 * @param deviceid
@@ -146,5 +143,7 @@ public class OperationController {
 		List<Track> tracks = trackService.getThreeDaysTrack();
 		return gson.toJson(tracks);
 	}
+	// ---------------------------------------------------------------------
+	
 	
 }
